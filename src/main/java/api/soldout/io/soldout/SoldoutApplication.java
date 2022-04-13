9c6236378b7c55edacfd06f49de0a6ce61bc2007
moranique <moranique@moraniques-MacBook-Air.local>
2022-04-13 21:05:40 +0900

    init

---
+++ src/main/java/api/soldout/io/soldout/SoldoutApplication.java
@@ -0,0 +1,13 @@
+package api.soldout.io.soldout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoldoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoldoutApplication.class, args);
+	}
+
+}
