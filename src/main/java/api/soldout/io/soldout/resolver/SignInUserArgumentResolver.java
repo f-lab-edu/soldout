721ec32cd7b91af854111fdd96607dc2c1fcedbd
kimcno3 <kimcno3@gmail.com>
2022-06-09 21:34:38 +0900

    SignInUserArgumentResolver 클래스 생성
    - 로그인한 회원 정보를 컨트롤러 매개변수로 UserDto로 가공하기 위한 리졸버

---
+++ src/main/java/api/soldout/io/soldout/resolver/SignInUserArgumentResolver.java
@@ -0,0 +1,50 @@
+package api.soldout.io.soldout.resolver;
+
+import api.soldout.io.soldout.annotation.SignInUser;
+import api.soldout.io.soldout.exception.NotSignInBrowserException;
+import api.soldout.io.soldout.mapper.UserMapper;
+import api.soldout.io.soldout.service.security.SecurityService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+/**
+ * .
+ */
+
+@Component
+@RequiredArgsConstructor
+public class SignInUserArgumentResolver implements HandlerMethodArgumentResolver {
+
+  private final SecurityService securityService;
+
+  private final UserMapper userMapper;
+
+  /**
+   * .
+   */
+
+  @Override
+  public boolean supportsParameter(MethodParameter parameter) {
+
+    return parameter.hasParameterAnnotation(SignInUser.class);
+
+  }
+
+  @Override
+  public Object resolveArgument(MethodParameter parameter,
+      ModelAndViewContainer mavContainer,
+      NativeWebRequest webRequest,
+      WebDataBinderFactory binderFactory) {
+
+    int userId = securityService.getCurrentUserId();
+
+    return userMapper.findById(userId);
+
+  }
+
+}
