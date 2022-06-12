60775f86fb81ee8afd57694c2de1eba7d8ef96b3
kimcno3 <kimcno3@gmail.com>
2022-06-13 03:20:24 +0900

    TradeServiceImpl 주입 객체 변경
    - SaleService -> SaleRepository

--- src/main/java/api/soldout/io/soldout/service/trade/TradeServiceImpl.java
+++ src/main/java/api/soldout/io/soldout/service/trade/TradeServiceImpl.java
@@ -6,8 +6,9 @@ import static api.soldout.io.soldout.dtos.entity.TradeDto.TradeStatus.TRADE_SIGN
 
 import api.soldout.io.soldout.dtos.entity.SaleDto;
 import api.soldout.io.soldout.dtos.entity.TradeDto;
+import api.soldout.io.soldout.exception.AlreadyMatchedException;
+import api.soldout.io.soldout.repository.sale.SaleRepository;
 import api.soldout.io.soldout.repository.trade.TradeRepository;
-import api.soldout.io.soldout.service.sale.SaleService;
 import java.time.LocalDateTime;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -23,14 +24,14 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class TradeServiceImpl implements TradeService {
 
-  private final SaleService saleService;
-
   private final TradeRepository tradeRepository;
+
+  private final SaleRepository saleRepository;
 
   @Override
   public void signTradeByOrder(int productId, int orderId, int size, int price) {
 
-    List<SaleDto> saleDtoList = saleService.findByProductId(productId);
+    List<SaleDto> saleDtoList = saleRepository.findByProductId(productId);
 
     int saleId = findMatchedSaleDto(saleDtoList, size, price);
 
@@ -46,7 +47,7 @@ public class TradeServiceImpl implements TradeService {
 
     tradeRepository.saveTrade(tradeDto);
 
-    saleService.updateSaleStatus(saleId, SALE_SIGNED);
+    saleRepository.updateSaleStatus(saleId, SALE_SIGNED);
 
   }
 
@@ -67,7 +68,7 @@ public class TradeServiceImpl implements TradeService {
   private int findMatchedSaleDto(List<SaleDto> saleDtoList, int size, int price) {
 
     // id가 기본적으로 인덱스로 사용되고 자동 증가로 입력되기 때문에 그 순서로 루프를 수행
-    // 판매 상태가 상태 판매 입찰중이면서 가격과 사이즈가 같은 녀석을 찾도록!!!!!!
+    // 판매 상태가 상태 판매 입찰중이면서 가격과 사이즈가 같은 녀석을 조회
     for (SaleDto tempSaleDto : saleDtoList) {
 
       if (tempSaleDto.getStatus().equals(SALE_PROGRESS)
@@ -80,7 +81,7 @@ public class TradeServiceImpl implements TradeService {
 
     }
 
-    throw new RuntimeException("어떠한 이유로 매칭되어야 할 판매 입찰가가 사라진 경우");
+    throw new AlreadyMatchedException("어떠한 이유로 매칭되어야 할 판매 입찰가가 사라진 경우");
 
   }
 
