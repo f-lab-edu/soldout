66a0c63aa753cb46407ca982b8c650618c6ca687
kimcno3 <kimcno3@gmail.com>
2022-06-13 03:10:35 +0900

    updateOrderStatus() 메소드 추가

--- src/main/java/api/soldout/io/soldout/service/order/OrderService.java
+++ src/main/java/api/soldout/io/soldout/service/order/OrderService.java
@@ -1,6 +1,7 @@
 package api.soldout.io.soldout.service.order;
 
 import api.soldout.io.soldout.dtos.entity.OrderDto;
+import api.soldout.io.soldout.dtos.entity.OrderDto.OrderStatus;
 import api.soldout.io.soldout.service.order.command.OrderCommand;
 import java.util.List;
 
@@ -15,5 +16,7 @@ public interface OrderService {
   List<OrderDto> findByUserId(String userId);
 
   List<OrderDto> findByProductId(String productId);
+
+  void updateOrderStatus(int saleId, OrderStatus status);
 
 }
