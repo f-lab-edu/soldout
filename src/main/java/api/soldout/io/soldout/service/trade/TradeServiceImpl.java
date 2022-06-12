f645cc9384dd50e91e33463d5f5c47f355146937
kimcno3 <kimcno3@gmail.com>
2022-06-12 23:14:35 +0900

    예외처리 로직 추가

--- src/main/java/api/soldout/io/soldout/service/trade/TradeServiceImpl.java
+++ src/main/java/api/soldout/io/soldout/service/trade/TradeServiceImpl.java
@@ -1,10 +1,10 @@
 package api.soldout.io.soldout.service.trade;
 
-import static api.soldout.io.soldout.dtos.entity.TradeDto.TradeStatus.DELIVER_FROM_SALE_COMPLETE;
 import static api.soldout.io.soldout.dtos.entity.TradeDto.TradeStatus.TRADE_SIGNING;
 
 import api.soldout.io.soldout.dtos.entity.SaleDto;
 import api.soldout.io.soldout.dtos.entity.TradeDto;
+import api.soldout.io.soldout.exception.AlreadyMatchedException;
 import api.soldout.io.soldout.repository.trade.TradeRepository;
 import api.soldout.io.soldout.service.sale.SaleService;
 import java.time.LocalDateTime;
@@ -12,7 +12,6 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
 
 /**
  * .
@@ -34,9 +33,12 @@ public class TradeServiceImpl implements TradeService {
 
     int saleId = findMatchedSaleDto(saleDtoList, size, price);
 
-    // 찾은 saleId가 이미 다른 사용자에 의해 거래 성사가 되었다면 trade 테이블에 해당 saleId가 있어야 한다.
+    // 찾은 saleId로 trade 테이블을 조회한 결과가 한개라도 있다면? 이미 매칭된 saleId 라는 의미
+    if (findBySaleId(saleId).size() > 0) {
 
+      throw new AlreadyMatchedException("이미 다른 구매 희망자와 매칭되었습니다. 다시 즉시 구매를 진행해주세요.");
 
+    }
 
     TradeDto tradeDto = TradeDto.builder()
         .productId(productId)
@@ -67,8 +69,8 @@ public class TradeServiceImpl implements TradeService {
   }
 
   private int findMatchedSaleDto(List<SaleDto> saleDtoList, int size, int price) {
-    // id가 기본적으로 인덱스로 사용되고 자동 증가로 입력되기 때문에 그 순서로 loop를 돈다.
-    // 생성 시기가 빠른 녀석을 찾기 위한 로직이 굳이 필요할지?
+
+    // id가 기본적으로 인덱스로 사용되고 자동 증가로 입력되기 때문에 그 순서로 루프를 수행 -> 생성 시기가 빠른 녀석을 찾기 위한 로직이 굳이 필요할지?
     for (SaleDto tempSaleDto : saleDtoList) {
 
       if (tempSaleDto.getSize() == size && tempSaleDto.getPrice() == price) {
@@ -79,6 +81,8 @@ public class TradeServiceImpl implements TradeService {
 
     }
 
+    // 그래도 찾고자 하는 saleId가 없다면?
+    // 어떠한 이유로 구매 버튼을 누른 시점에 존재했던 SaleId가 없어진 경우
     throw new RuntimeException("판매 입찰 기간이 지나서 갑작스럽게 매칭 가능한 판매자가 사라진 경우");
 
   }
