ae31b343f05b18b3be5275c1e3f0ea0489ee3829
kimcno3 <kimcno3@gmail.com>
2022-06-12 17:47:43 +0900

    즉시 구매 테스트 코드 생성

--- src/test/java/api/soldout/io/soldout/service/order/OrderServiceImplTest.java
+++ src/test/java/api/soldout/io/soldout/service/order/OrderServiceImplTest.java
@@ -1,14 +1,21 @@
 package api.soldout.io.soldout.service.order;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
 import api.soldout.io.soldout.repository.order.OrderRepository;
+import api.soldout.io.soldout.service.order.command.OrderCommand;
 import api.soldout.io.soldout.service.trade.TradeService;
-import lombok.extern.slf4j.Slf4j;
+import api.soldout.io.soldout.util.enums.OrderType;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-@Slf4j
 @ExtendWith(MockitoExtension.class)
 class OrderServiceImplTest {
 
@@ -24,6 +31,26 @@ class OrderServiceImplTest {
   void init() {
 
     orderService = new OrderServiceImpl(orderRepository, tradeService);
+
+  }
+
+  @Test
+  @DisplayName("즉시 구매 테스트")
+  void orderNowTest() {
+    // given
+    OrderCommand command = new OrderCommand(
+        1, 1, 250, 100000, 3, OrderType.ORDER_NOW
+    );
+
+    // when
+    orderService.orderNow(command);
+
+    // then
+    verify(orderRepository).saveOrder(any());
+    verify(orderRepository, times(1)).saveOrder(any());
+
+    verify(tradeService).signTradeByOrder(anyInt(), anyInt(), anyInt(), anyInt());
+    verify(tradeService, times(1)).signTradeByOrder(anyInt(), anyInt(), anyInt(), anyInt());
 
   }
 
