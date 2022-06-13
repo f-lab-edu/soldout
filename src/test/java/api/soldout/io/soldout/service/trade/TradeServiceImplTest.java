80a19b7ae2ffc39b44a0b749f54d6c847371aab8
kimcno3 <kimcno3@gmail.com>
2022-06-13 17:16:07 +0900

    피드백 반영
    - 생성자 메소드 삭제
    - mock 객체 직접 생성 (mock())
    - 에러 발생 요인 해결(내부 메서드 동작에 필요한 객체 생성)

--- src/test/java/api/soldout/io/soldout/service/trade/TradeServiceImplTest.java
+++ src/test/java/api/soldout/io/soldout/service/trade/TradeServiceImplTest.java
@@ -1,20 +1,27 @@
 package api.soldout.io.soldout.service.trade;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import api.soldout.io.soldout.dtos.entity.SaleDto;
+import api.soldout.io.soldout.dtos.entity.SaleDto.SaleStatus;
 import api.soldout.io.soldout.repository.sale.SaleRepository;
+import api.soldout.io.soldout.repository.sale.SaleRepositoryImpl;
 import api.soldout.io.soldout.repository.trade.TradeRepository;
-import api.soldout.io.soldout.service.sale.SaleService;
-import org.junit.jupiter.api.Assertions;
+import api.soldout.io.soldout.repository.trade.TradeRepositoryImpl;
+import api.soldout.io.soldout.util.enums.SaleType;
+import java.util.ArrayList;
+import java.util.List;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
@@ -22,44 +29,67 @@ class TradeServiceImplTest {
 
   private TradeService tradeService;
 
-  @Mock
   private SaleRepository saleRepository;
 
-  @Mock
   private TradeRepository tradeRepository;
-
-  private TradeService tradeService(
-      TradeRepository tradeRepository, SaleRepository saleRepository) {
-
-    return new TradeServiceImpl(tradeRepository, saleRepository);
-
-  }
-
 
   @BeforeEach
   void init() {
 
-    tradeService = tradeService(tradeRepository, saleRepository);
+    saleRepository = mock(SaleRepositoryImpl.class);
+
+    tradeRepository = mock(TradeRepositoryImpl.class);
+
+    tradeService = new TradeServiceImpl(tradeRepository, saleRepository);
 
   }
 
   @Test
-  @DisplayName("즉시 구매 요청에 따른 거래 채결 텟스트")
-  @Disabled
+  @DisplayName("즉시 구매 요청에 따른 거래 채결 테스트")
   void signTradeByOrderTest() {
     // given
     int productId = 1;
     int orderId = 1;
     int size = 250;
     int price = 100000;
+
+    SaleDto saleDto = SaleDto.builder()
+        .id(1)
+        .productId(productId)
+        .userId(1)
+        .size(size)
+        .price(price)
+        .date(3)
+        .type(SaleType.SALE_BID)
+        .status(SaleStatus.BID_PROGRESS)
+        .build();
+
+    List<SaleDto> saleDtoList = new ArrayList<>();
+    saleDtoList.add(saleDto);
+
+
     // when
-    // tradeService.signTradeByOrder(productId, orderId, size, price);
+    when(saleRepository.findByProductId(productId))
+        .thenReturn(saleDtoList);
 
-    //then
-    // verify(saleRepository).findByProductId(productId);
-    // verify(saleRepository, times(1)).findByProductId(productId);
+    tradeService.signTradeByOrder(productId, orderId, size, price);
 
-    // verify(tradeRepository).saveTrade(any());
-    // verify(tradeRepository, times(1)).saveTrade(any());
+    // then
+    assertThat(saleRepository.findByProductId(productId))
+        .isEqualTo(saleDtoList);
+
+    verify(tradeRepository)
+        .saveTrade(any());
+
+    verify(tradeRepository, times(1))
+        .saveTrade(any());
+
+    verify(saleRepository)
+        .updateSaleStatus(saleDto.getId(), SaleStatus.MATCHING_COMPLETE);
+
+    verify(saleRepository, times(1))
+        .updateSaleStatus(saleDto.getId(), SaleStatus.MATCHING_COMPLETE);
+
   }
+
 }
