7f2e16253170b0d7de5fd1a00417b9f78e5ae407
kimcno3 <kimcno3@gmail.com>
2022-06-20 23:22:58 +0900

    @EnableRedisHttpSession 어노테이션 주석처리

--- src/main/java/api/soldout/io/soldout/SoldoutApplication.java
+++ src/main/java/api/soldout/io/soldout/SoldoutApplication.java
@@ -2,11 +2,13 @@ package api.soldout.io.soldout;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 /**
  * .
  */
 
+//@EnableRedisHttpSession
 @SpringBootApplication
 public class SoldoutApplication {
   public static void main(String[] args) {
